098ec5d1faecbfc0544f7abb2c41dc7f30a4b346
AssBreaker233 <119254619+AssBreaker233@users.noreply.github.com>
2022-11-27 14:04:27 -0600

    Add files via upload

---
+++ tabla.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void imprimetabla(int *A){
+	int i;
+	for(i=0;i<=255;i++){
+	  if((i%5)==0){
+	  	printf("\n");
+	  }
+	  printf("%d - - - [%d]\t\t",i,A[i]);
+	}
+}
+void frecuencias(int *A){
+	int n;
+	FILE *pArchivo;
+    pArchivo = fopen("nombre.png", "rb"); //se coloca el nombre de archivo y extensión
+    if(pArchivo==NULL){
+    	printf("\n ***** ERROR *****");
+    }else{
+    	printf("\n ***** Abierto *****");
+    	while(!feof(pArchivo)){
+    		fread(&n,1,1,pArchivo);
+    		 A[n]++;
+    	}
+    fclose(pArchivo);
+    }
+}
+
+int main(){
+	int A[255]={};
+	frecuencias(A);
+	imprimetabla(A); //opcional
+	return 0;
+}
